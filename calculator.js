e9c9a3678c5d5a30a01a2851db2e1631195ffd87
yash-rapidops <yash.gohel@rapidops.com>
2022-09-26 12:05:59 +0530

    addded the calculator file for demo

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const num1 = 5;
+const num2 = 3;
+const add = (num1,num2)=>{
+    const sum = num1 + num2;
+    console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+}
+add(num1,num2)
