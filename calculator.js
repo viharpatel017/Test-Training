f3a4eca77ae11ce7c8a56424b7b984c1a3bd28eb
yash-rapidops <yash.gohel@rapidops.com>
2022-09-26 15:13:32 +0530

    Inital commmit

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const num1 = 5;
+const num2 = 3;
+const add = (num1, num2) => {
+    const sum = num1 + num2;
+    console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+}
+add(num1,num2);
+const multi = (num1,num2)=>{
+    const sum = num1 * num2;
+    console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+}
+multi(num1,num2);
+const multiplication = (num1, num2) => {
+    const multiply = num1 * num2;
+    console.log('The multiplication of ' + num1 + ' and ' + num2 + ' is: ' + multiply);
+}
